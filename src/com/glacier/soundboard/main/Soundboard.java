3aad89d8a0a5429319e5e2badac9567acb113c2f
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2018-11-28 13:48:10 -0600

    Full Change
    This was a bit of a small change for a full branch, but I didn't want to just lump it into the deleting things branch, since that's bound to be a LOT of change.

--- src/com/glacier/soundboard/main/Soundboard.java
+++ src/com/glacier/soundboard/main/Soundboard.java
@@ -4,6 +4,7 @@ import com.glacier.soundboard.handlers.AddPhotoHandler;
 import com.glacier.soundboard.handlers.AddSoundsHandler;
 import com.glacier.soundboard.handlers.ChooseABoard;
 import com.glacier.soundboard.handlers.ShowHowTo;
+import com.glacier.soundboard.handlers.ShowMakeSounds;
 import com.glacier.soundboard.util.Constants;
 import com.glacier.soundboard.util.UtilityMethods;
 
@@ -21,17 +22,21 @@ public class Soundboard extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		VBox wrapThings = new VBox();
 		HBox buttons = new HBox();
+		HBox buttonsTwo = new HBox();
 		Text question = new Text("What would you like to do?");
 		Button btSound = new Button("Add Sounds");
 		Button btPhoto = new Button("Add Photos");
 		Button btChoose = new Button("Choose a Board");
 		Button btHow = new Button("Help me!");
+		Button btStart = new Button("Start Soundboard");
 		btSound.setOnAction(new AddSoundsHandler());
 		btPhoto.setOnAction(new AddPhotoHandler());
 		btChoose.setOnAction(new ChooseABoard());
 		btHow.setOnAction(new ShowHowTo());
+		btStart.setOnAction(new ShowMakeSounds());
 		buttons.getChildren().addAll(btSound,btPhoto,btChoose,btHow);
-		wrapThings.getChildren().addAll(question,buttons);
+		buttonsTwo.getChildren().addAll(btStart);
+		wrapThings.getChildren().addAll(question,buttons,buttonsTwo);
 		Scene primaryScene = new Scene(wrapThings,Constants.openingWidth,Constants.openingHeight);
 		primaryStage.setScene(primaryScene);
 		primaryStage.setTitle("Soundboard Menu");
